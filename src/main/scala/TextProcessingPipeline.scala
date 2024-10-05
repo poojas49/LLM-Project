b91a7945f16c2f700eb5d806d1219116b4d0c3e2
poojas49 <pshin8@uic.edu>
2024-10-05 08:16:47 -0500

    modified hadoop configuration for sharding of data

--- src/main/scala/TextProcessingPipeline.scala
+++ src/main/scala/TextProcessingPipeline.scala
@@ -1,19 +1,46 @@
+import com.typesafe.config.ConfigFactory
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
+
+import scala.util.{Failure, Success, Try}
 
 object TextProcessingPipeline {
   def main(args: Array[String]): Unit = {
-    if (args.length != 2) {
-      System.err.println("Usage: TextProcessingPipeline <input path> <output path>")
-      System.exit(-1)
+    args.toList match {
+      case inputPath :: outputBasePath :: Nil =>
+        processFiles(new Path(inputPath), new Path(outputBasePath))
+      case _ =>
+        System.err.println("Usage: TextProcessingPipeline <input path> <output path>")
+        System.exit(-1)
     }
+  }
 
-    TextShardingJob.runJob(args(0))
+  def processFiles(inputPath: Path, outputBasePath: Path): Unit = {
+    val result = Try(ConfigFactory.load())
+      .map { config =>
+        (
+          config.getLong("hadoop.split.size.mb"),
+          config.getInt("hadoop.num.reducers"),
+        )
+      }
+      .map { case (dataShardSizeInMB, numReducers) =>
+        val conf = new Configuration()
+        conf.setLong(FileInputFormat.SPLIT_MAXSIZE, dataShardSizeInMB * 1024 * 1024)
+        conf.setInt("mapreduce.job.reduces", numReducers)
+        conf
+      }
+      .flatMap(conf => runPipeline(conf, inputPath, outputBasePath))
 
-    val inputPath = new Path(args(0))
-    val outputBasePath = new Path(args(1))
-    val conf = new Configuration()
+    result match {
+      case Success(output) => println(s"Pipeline completed. Final output: $output")
+      case Failure(exception) =>
+        System.err.println(s"Pipeline failed: ${exception.getMessage}")
+        exception.printStackTrace()
+    }
+  }
 
+  def runPipeline(conf: Configuration, inputPath: Path, outputBasePath: Path): Try[Path] = {
     val jobOutputs = List(
       ("tokenization", TokenizationJob.runJob),
       ("sliding_window", SlidingWindowJob.runJob),
@@ -21,18 +48,21 @@ object TextProcessingPipeline {
       ("semantic_similarity", SemanticSimilarityJob.runJob)
     )
 
-    jobOutputs.foldLeft(inputPath) { case (prevOutput, (jobName, runJob)) =>
-      val output = new Path(outputBasePath, jobName)
-      runJob(conf, prevOutput, output)
-      output
+    val pipelineResult = jobOutputs.foldLeft(Try(inputPath)) {
+      case (prevOutputTry, (jobName, runJob)) =>
+        prevOutputTry.flatMap { prevOutput =>
+          val output = new Path(outputBasePath, jobName)
+          Try(runJob(conf, prevOutput, output)).map(_ => output)
+        }
     }
 
-    // Run the Statistics Collater Job
-    val tokenizationOutput = new Path(outputBasePath, "tokenization")
-    val semanticSimilarityOutput = new Path(outputBasePath, "semantic_similarity")
-    val finalOutput = new Path(outputBasePath, "statistics_collated")
-    StatisticsCollaterJob.runJob(conf, tokenizationOutput, semanticSimilarityOutput, finalOutput)
-
-    println(s"Pipeline completed. Final output: $finalOutput")
+    pipelineResult.flatMap { _ =>
+      val tokenizationOutput = new Path(outputBasePath, "tokenization")
+      val semanticSimilarityOutput = new Path(outputBasePath, "semantic_similarity")
+      val finalOutput = new Path(outputBasePath, "statistics_collated")
+      conf.setInt("mapreduce.job.reduces", 1)
+      Try(StatisticsCollaterJob.runJob(conf, tokenizationOutput, semanticSimilarityOutput, finalOutput))
+        .map(_ => finalOutput)
+    }
   }
 }
